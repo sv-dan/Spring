f75821210d7ba29dfeaaa3224fc6f0ad37479174
sv-dan <sav4enkov.d@mail.ru>
2025-06-04 10:27:06 +0300

    spring 2.1.2 - ломбок

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,15 +1,11 @@
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
-
+@Getter
+@Setter
 @Component
 public class Cat {
     private String name;
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public String toString() {
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,10 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class HelloWorld {
- 
+
     private String message;
- 
-    public String getMessage() {
-        return message;
-    }
- 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-     
+
 }
